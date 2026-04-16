be5297d836992433bbdec7e105c4f723c5072989
ZeymorG <zeymorgrefa@gmail.com>
2026-04-15 19:23:23 -0500

    Calculadora basica

--- EJMP.cpp
+++ EJMP.cpp
@@ -1,8 +1,36 @@
-
 #include <iostream>
- 
-int main () 
-{
-    std::cout << "Hola, FISOW;
+
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Ingrese operador (+, -, *, /): ";
+    cin >> op;
+    cout << "Ingrese dos numeros: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 + num2;
+            break;
+        case '-':
+            cout << num1 - num2;
+            break;
+        case '*':
+            cout << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << num1 / num2;
+            else
+                cout << "Error: Division por cero";
+            break;
+        default:
+            cout << "Operador no valido";
+            break;
+    }
+
     return 0;
 }
